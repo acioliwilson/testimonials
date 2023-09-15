91eca08924afc9fddd6d1465a40194f44d791586
Wilson Acioli <acioliwilson@gmail.com>
2023-09-15 18:06:19 -0300

    fix: update api route to /api/v1

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const testimonialSchema = new mongoose.Schema({
 
 const Testimonial = mongoose.model('Testimonial', testimonialSchema);
 
-app.get('/testimonials', async (req, res) => {
+app.get('/api/v1/testimonials', async (req, res) => {
  const testimonials = await Testimonial.find();
  res.json(testimonials);
 });
 
-app.post('/testimonials', async (req, res) => {
+app.post('/api/v1/testimonials', async (req, res) => {
  const testimonial = new Testimonial(req.body);
  await testimonial.save();
  res.status(201).json(testimonial);
