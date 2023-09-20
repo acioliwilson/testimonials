1ebe75202a8b09c2dbe290ac404885524a1d358f
Wilson Acioli <acioliwilson@gmail.com>
2023-09-20 09:20:16 -0300

    fix: update vercel.json file and allow cors origin in api main file

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(cors({
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
